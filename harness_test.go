5f0be293a15d29627eedf95cf7c95654457b1838
David Mitchell <monch1962@gmail.com>
2020-09-25 09:57:38 +0000

    Incomplete

--- harness_test.go
+++ harness_test.go
@@ -13,7 +13,7 @@ func TestFailing(t *testing.T) {
 	t.Fail()
 }
 
-func readEnvVars() (string, string, string, string, string, string, string, string) {
+func readEnvVars() (string, string, string, string, string, string, string, string, string, string) {
 	publishAmqpServer := os.Getenv("PUBLISH_AMQP_SERVER")
 	publishAmqpTCPPort := os.Getenv("PUBLISH_AMQP_SERVER_TCP")
 	if publishAmqpTCPPort == "" {
@@ -27,15 +27,22 @@ func readEnvVars() (string, string, string, string, string, string, string, stri
 	if subscribeAmqpTCPPort == "" {
 		subscribeAmqpTCPPort = publishAmqpTCPPort
 	}
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
+	publishUsername := os.Getenv("PUBLISH_USERNAME")
+	publishPassword := os.Getenv("PUBLISH_PASSWORD")
+	subscribeUsername := os.Getenv("SUBSCRIBE_USERNAME")
+	if subscribeUsername == "" {
+		subscribeUsername = publishUsername
+	}
+	subscribePassword := os.Getenv("SUBSCRIBE_PASSWORD")
+	if subscribePassword == "" {
+		subscribePassword = publishPassword
+	}
 	publishQ := os.Getenv("PUBLISH_Q")
 	subscribeQ := os.Getenv("SUBSCRIBE_Q")
-	return publishAmqpServer,publishAmqpTCPPort,subscribeAmqpServer, subscribeAmqpTCPPort,username,password,publishQ,subscribeQ
+	return publishAmqpServer,publishAmqpTCPPort,publishUsername, publishPassword, subscribeAmqpServer, subscribeAmqpTCPPort,subscribeUsername,subscribePassword,publishQ,subscribeQ
 }
 
 func SendMsg(ch *amqp.Channel, queue string, payload string) (string, error) {
-	publishAmqpServer, publishAmqpServerPort, subscribeAmqpServer, subscribeAmqpServerPort, username, password, publishQ, subscribeQ := readEnvVars()
 	q, err := ch.QueueDeclare(
 		queue, // name
 		false,   // durable
@@ -65,23 +72,8 @@ func SendMsg(ch *amqp.Channel, queue string, payload string) (string, error) {
 }
 
 func RecvMsg(ch *amqp.Channel, queue string) (string, error) {
-	_, _, subscribeAmqpServer, subscribeAmqpServerPort, username, password, _, subscribeQ := readEnvVars()
-	amqpURI := fmt.Sprintf("amqp://%s:%s@%s:%s/", username, password,subscribeAmqpServer, subscribeAmqpServerPort)
-	//conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	conn, err := amqp.Dial(amqpURI)
-	if err != nil {
-		return "", fmt.Errorf("Failed to connect to RabbitMQ")
-	}
-	defer conn.Close()
-
-	ch, err = conn.Channel()
-	if err != nil {
-		return "", fmt.Errorf("Failed to open a channel")
-	}
-	defer ch.Close()
-
 	q, err := ch.QueueDeclare(
-		subscribeQ, // name
+		queue, // name
 		false,   // durable
 		false,   // delete when unused
 		false,   // exclusive
@@ -120,11 +112,13 @@ func RecvMsg(ch *amqp.Channel, queue string) (string, error) {
 }
 
 func TestAMQP(t *testing.T) {
-	sendQueue := "Hello"
+	publishAmqpServer, publishAmqpServerPort, publishUsername, publishPassword, subscribeAmqpServer, subscribeAmqpServerPort, subscribeUsername, subscribePassword, publishQ, subscribeQ := readEnvVars()
+	publishAmqpURI := fmt.Sprintf("amqp://%s:%s@%s:%s/", publishUsername, publishPassword, publishAmqpServer, publishAmqpServerPort)
+
 	sendPayload := "Hello"
-	recvQueue := "Hello"
 	expectRecvPayload := "Hello"
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	conn, err := amqp.Dial(publishAmqpURI)
 	if err != nil {
 		t.Fatalf("Failed to connect to RabbitMQ: %v\n", err)
 	}
@@ -134,14 +128,32 @@ func TestAMQP(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to open a channel: %v\n", err)
 	}
-
 	defer ch.Close()
 
-	_, err = SendMsg(ch, sendQueue, sendPayload)
+	_, err = SendMsg(ch,publishQ, sendPayload)
 	if err != nil {
 		t.Fatalf("Failed to send message '%s': %v\n", sendPayload, err)
 	}
-	responsePayload, err := RecvMsg(ch, recvQueue)
+
+	subscribeAmqpURI := fmt.Sprintf("amqp://%s:%s@%s:%s/", subscribeUsername, subscribePassword, subscribeAmqpServer, subscribeAmqpServerPort)
+	if subscribeAmqpURI != publishAmqpURI {
+		// we're reading from a different queue server; close the existing connection and open a new one
+		ch.Close()
+		conn.Close()
+		conn, err := amqp.Dial(subscribeAmqpURI)
+		if err != nil {
+			t.Fatalf("Failed to connect to RabbitMQ: %v\n", err)
+		}
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+		if err != nil {
+			t.Fatalf("Failed to open a channel: %v\n", err)
+		}
+		defer ch.Close()
+	}
+
+	responsePayload, err := RecvMsg(ch, subscribeQ)
 	if responsePayload != expectRecvPayload {
 		t.Logf("Expected response '%s' doesn't match actual response '%s'\n", expectRecvPayload, responsePayload)
 		t.Fail()
