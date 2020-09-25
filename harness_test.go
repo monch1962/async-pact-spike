1ec39b001fcd4663a6484cb65d7a9f60695108f2
David Mitchell <monch1962@gmail.com>
2020-09-25 12:21:26 +0000

    First working version

--- harness_test.go
+++ harness_test.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"time"
 
-	"github.com/streadway/amqp"
 	"log"
 	"os"
 	"testing"
+
+	"github.com/streadway/amqp"
 )
 
-func TestFailing(t *testing.T) {
+/*func TestFailing(t *testing.T) {
 	t.Fail()
-}
+}*/
 
-func readEnvVars() (string, string, string, string, string, string, string, string, string, string) {
+func readEnvVars() (string, string, string, string, string, string, string, string, string, string, string, string, string, int64) {
+	timeout, err := strconv.ParseInt(os.Getenv("TIMEOUT"), 10, 0)
+	if err != nil {
+
+	}
+	protocol := os.Getenv("PROTOCOL")
+	if protocol != "amqp" && protocol != "amqps" {
+	}
+
 	publishAmqpServer := os.Getenv("PUBLISH_AMQP_SERVER")
 	publishAmqpTCPPort := os.Getenv("PUBLISH_AMQP_SERVER_TCP")
 	if publishAmqpTCPPort == "" {
-		publishAmqpTCPPort = "5672"
+		if protocol == "amqp" {
+			publishAmqpTCPPort = "5672"
+		} else {
+			publishAmqpTCPPort = "5671"
+		}
 	}
+	publishURISuffix := os.Getenv("PUBLISH_URI_SUFFIX")
 	subscribeAmqpServer := os.Getenv("SUBSCRIBE_AMQP_SERVER")
 	if subscribeAmqpServer == "" {
 		subscribeAmqpServer = publishAmqpServer
@@ -26,6 +42,10 @@ func readEnvVars() (string, string, string, string, string, string, string, stri
 	subscribeAmqpTCPPort := os.Getenv("SUBSCRIBE_AMQP_SERVER_TCP")
 	if subscribeAmqpTCPPort == "" {
 		subscribeAmqpTCPPort = publishAmqpTCPPort
+	}
+	subscribeURISuffix := os.Getenv("SUBSCRIBE_URI_SUFFIX")
+	if subscribeURISuffix == "" {
+		subscribeURISuffix = publishURISuffix
 	}
 	publishUsername := os.Getenv("PUBLISH_USERNAME")
 	publishPassword := os.Getenv("PUBLISH_PASSWORD")
@@ -39,20 +59,21 @@ func readEnvVars() (string, string, string, string, string, string, string, stri
 	}
 	publishQ := os.Getenv("PUBLISH_Q")
 	subscribeQ := os.Getenv("SUBSCRIBE_Q")
-	return publishAmqpServer,publishAmqpTCPPort,publishUsername, publishPassword, subscribeAmqpServer, subscribeAmqpTCPPort,subscribeUsername,subscribePassword,publishQ,subscribeQ
+	return protocol, publishAmqpServer, publishAmqpTCPPort, publishUsername, publishPassword, publishURISuffix, subscribeAmqpServer, subscribeAmqpTCPPort, subscribeURISuffix, subscribeUsername, subscribePassword, publishQ, subscribeQ, timeout
 }
 
 func SendMsg(ch *amqp.Channel, queue string, payload string) (string, error) {
+	log.Printf("Attempting to write payload '%s' to queue '%s'\n", payload, queue)
 	q, err := ch.QueueDeclare(
 		queue, // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	if err != nil {
-		return "",fmt.Errorf("Failed to declare a queue:%v", err)
+		return "", fmt.Errorf("Failed to declare a queue:%v", err)
 	}
 
 	body := payload
@@ -66,22 +87,23 @@ func SendMsg(ch *amqp.Channel, queue string, payload string) (string, error) {
 			Body:        []byte(body),
 		})
 	if err != nil {
-		return "",fmt.Errorf("Failed to publish a message: %v", err)
+		return "", fmt.Errorf("Failed to publish a message: %v", err)
 	}
 	return body, nil
 }
 
-func RecvMsg(ch *amqp.Channel, queue string) (string, error) {
+func RecvMsg(ch *amqp.Channel, queue string, timeout int64) (string, error) {
+	log.Printf("Reading from queue '%s'\n", queue)
 	q, err := ch.QueueDeclare(
 		queue, // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	if err != nil {
-		return "", fmt.Errorf( "Failed to declare a queue")
+		return "", fmt.Errorf("Failed to declare a queue")
 	}
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -95,29 +117,45 @@ func RecvMsg(ch *amqp.Channel, queue string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("Failed to register a consumer")
 	}
-
-	forever := make(chan bool)
+	//log.Printf("msgs: %v\n", msgs)
 	var msg string
 
-	go func() {
-		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+	duration := time.Duration(timeout) * time.Millisecond
+	timer := time.NewTimer(duration)
+	for {
+		select {
+		case d := <-msgs:
+			timer.Reset(duration)
+			log.Printf("Received a message: '%s'\n", d.Body)
 			msg = string(d.Body)
+		case <-timer.C:
+			log.Println("Timeout !")
+			return "", fmt.Errorf("Timeout waiting for message to appear")
 		}
-	}()
 
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
-	return msg, nil
+		return msg, nil
+	}
+
+}
+
+type PactDetail struct {
+	testName string
+	reqBody  string
+	respBody interface{}
 }
 
 func TestAMQP(t *testing.T) {
-	publishAmqpServer, publishAmqpServerPort, publishUsername, publishPassword, subscribeAmqpServer, subscribeAmqpServerPort, subscribeUsername, subscribePassword, publishQ, subscribeQ := readEnvVars()
-	publishAmqpURI := fmt.Sprintf("amqp://%s:%s@%s:%s/", publishUsername, publishPassword, publishAmqpServer, publishAmqpServerPort)
+	protocol, publishAmqpServer, publishAmqpServerPort, publishUsername, publishPassword, publishURISuffix, subscribeAmqpServer, subscribeAmqpServerPort, subscribeURISuffix, subscribeUsername, subscribePassword, publishQ, subscribeQ, timeout := readEnvVars()
 
-	sendPayload := "Hello"
-	expectRecvPayload := "Hello"
-
+	publishAmqpURI := fmt.Sprintf("%s://%s:%s@%s:%s/%s", protocol, publishUsername, publishPassword, publishAmqpServer, publishAmqpServerPort, publishURISuffix)
+	///////
+	var testCases = []PactDetail{
+		{"TestA", "hello", "hello"},
+	}
+	sendPayload := testCases[0].reqBody
+	expectRecvPayload := testCases[0].respBody
+	///////
+	t.Logf("Publishing to %s\n", publishAmqpURI)
 	conn, err := amqp.Dial(publishAmqpURI)
 	if err != nil {
 		t.Fatalf("Failed to connect to RabbitMQ: %v\n", err)
@@ -130,12 +168,12 @@ func TestAMQP(t *testing.T) {
 	}
 	defer ch.Close()
 
-	_, err = SendMsg(ch,publishQ, sendPayload)
+	_, err = SendMsg(ch, publishQ, sendPayload)
 	if err != nil {
 		t.Fatalf("Failed to send message '%s': %v\n", sendPayload, err)
 	}
 
-	subscribeAmqpURI := fmt.Sprintf("amqp://%s:%s@%s:%s/", subscribeUsername, subscribePassword, subscribeAmqpServer, subscribeAmqpServerPort)
+	subscribeAmqpURI := fmt.Sprintf("amqps://%s:%s@%s:%s/%s", subscribeUsername, subscribePassword, subscribeAmqpServer, subscribeAmqpServerPort, subscribeURISuffix)
 	if subscribeAmqpURI != publishAmqpURI {
 		// we're reading from a different queue server; close the existing connection and open a new one
 		ch.Close()
@@ -155,9 +193,12 @@ func TestAMQP(t *testing.T) {
 	defer conn.Close()
 	defer ch.Close()
 
-	responsePayload, err := RecvMsg(ch, subscribeQ)
+	t.Logf("Subscribing to %s\n", subscribeAmqpURI)
+	responsePayload, err := RecvMsg(ch, subscribeQ, timeout)
 	if expectRecvPayload != responsePayload {
 		t.Logf("Expected response '%s' doesn't match actual response '%s'\n", expectRecvPayload, responsePayload)
 		t.Fail()
+	} else {
+		t.Log("Expected payload matches actual payload")
 	}
 }
